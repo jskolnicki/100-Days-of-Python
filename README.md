9ea0754e7a41fd483e96636d025b4ed44e11841f
jskolnicki <33360411+jskolnicki@users.noreply.github.com>
2023-02-05 19:00:48 -0700

    Day 38 - sheety and nutritionix api

--- README.md
+++ README.md
@@ -6,10 +6,23 @@ This course is designed to provide a foundational understanding of Python. I rea
 Examples include:
 
 Day 11 - Adding a rolling bank account so that you can play a real blackjack game
+
 Day 22 - Adding a custom photo as the pong ball and speeding up the game as it goes (Pong)
+
 Day 23 - Creating a "cheat code" to move to the next level automatically (Frogger)
+
 Day 24 - Not dying in the snake game when it hits the edge (Snake game)
+
 Day 28 - Making my own Pomodoro timer with my friend Pat's NFT
+
 Day 28 - Creating my own tool to count the number of times that my coworker David and I say the word "dammit" (dammits.py)
+
 Day 31 - Not using the timer aspect of flashcards and instead adding a flip card button. Designate a name at beginning so that multiple people can save their progress
+
 Day 32 - Took the same concept into the workplace. Having Goggins motivational quotes sent to the #team-closers channel in Slack
+
+Day 36 - Future to-do: Get stock data through API and then send a text message alert through Twilio
+
+Day 37 - Uses the pixela API to create habit tracking. Future to-do: automate this to run when I do something that I want to track
+
+Day 38 - Used my own bearer token on the Sheety API getting info from the Nutritionix API
---
+++ day-38-google-api-workout-tracker/main.py
@@ -0,0 +1,58 @@
+import requests
+from datetime import datetime
+import os
+
+GOOGLE_SHEET_NAME = 'workout'
+GENDER = 'male'
+WEIGHT_KG = 87
+HEIGHT_CM = 188
+AGE = 28
+
+APP_ID = os.environ["PERSONAL_SHEETY_APP_ID"]
+APP_KEY = os.environ["PERSONAL_SHEETY_API_KEY"]
+SHEETY_BEARER_TOKEN = os.environ["PERSONAL_SHEETY_BEARER_TOKEN"]
+
+bearer_headers = {'Authorization': SHEETY_BEARER_TOKEN}
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+date = datetime.strftime(datetime.now(), '%Y-%b-%d')
+time = datetime.strftime(datetime.now(), '%X')
+
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+
+result = response.json()
+
+
+sheet_endpoint = 'https://api.sheety.co/2e8654f86ce23eb2d2ddf106a5a2b545/workoutTracking/workouts'
+
+
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        GOOGLE_SHEET_NAME: {
+            "date": date,
+            "time": time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs, headers=bearer_headers)
